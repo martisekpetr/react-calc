c65ef00154b60a0a355141c602fdc12c3cace46b
Petr Martišek <martisekpetr@gmail.com>
2017-09-12 11:56:47 +0200

    State and methods in index.js

---
+++ src/Calculator.js
@@ -0,0 +1,31 @@
+import React from 'react'
+// import PropTypes from 'prop-types'
+
+
+const Calculator = ({display, appendToDisplay, performOperation, getResult}) => {
+    return (
+        <div>
+            <h3>{display}</h3>
+            <button onClick={() => appendToDisplay(1)}>1</button>
+            <button onClick={() => appendToDisplay(2)}>2</button>
+            <button onClick={() => appendToDisplay(3)}>3</button>
+            <br />
+            <button onClick={() => appendToDisplay(4)}>4</button>
+            <button onClick={() => appendToDisplay(5)}>5</button>
+            <button onClick={() => appendToDisplay(6)}>6</button>
+            <br />
+            <button onClick={() => appendToDisplay(7)}>7</button>
+            <button onClick={() => appendToDisplay(8)}>8</button>
+            <button onClick={() => appendToDisplay(9)}>9</button>
+            <br />
+            <button onClick={() => performOperation('add')}>+</button>
+            <button onClick={() => performOperation('subtract')}>-</button>
+            <button onClick={() => performOperation('multiply')}>*</button>
+            <button onClick={() => performOperation('divide')}>/</button>
+            <button onClick={getResult}>=</button>
+        </div>
+);
+};
+
+
+export default Calculator
