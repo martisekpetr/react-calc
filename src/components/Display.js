ed084319087132f9eab8fb750394433afba1f18e
Petr Martišek <martisekpetr@gmail.com>
2017-09-12 16:49:26 +0200

    Remade with react-redux

---
+++ src/components/Display.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+
+const Display = ({display}) => {
+    return <h3>{display}</h3>;
+};
+
+Display.propTypes = {
+    display : PropTypes.number.isRequired,
+};
+
+const mapStateToProps = (state) => ({
+    display : state.display,
+
+});
+
+
+export default connect(mapStateToProps, undefined)(Display);
