2abfcaf0246561265939ac3dffce3e4eb6db7c57
Branislav Holý <branislav@salsitasoft.com>
2017-09-12 14:33:27 +0200

    Added redux support

--- src/index.js
+++ src/index.js
@@ -1,8 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import { createStore } from 'redux';
+
 import registerServiceWorker from './registerServiceWorker';
+
+import App from './components/App';
+import rootReducer from './reducers/rootReducer';
+
+// const store = createStore(
+//   rootReducer,
+//   window.devToolsExtension ? window.devToolsExtension() : v => v
+// );
+
+// Use:
+// store.getState() to get the state
+// store.dispatch to dispatch an action
 
 const state = {
     display : 0,
@@ -68,7 +80,7 @@ const render = (appState) =>{
         document.getElementById('root'));
 };
 
-
+// store.subscribe(render);
 render(state);
 
 
